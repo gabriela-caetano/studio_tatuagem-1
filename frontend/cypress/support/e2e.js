0550a75ab827dde300326b4c6e893f37ff09e424
Gabriela Aparecida Silva Caetano <166453006+gabriela-caetano@users.noreply.github.com>
2025-11-28 18:18:27 -0300

    Delete frontend/cypress directory

--- frontend/cypress/support/e2e.js
+++
@@ -1,8 +0,0 @@
-// Importa comandos customizados (crie o arquivo commands.js abaixo) e configurações globais.
-import './commands';
-
-// Exemplo de configuração global (opcional)
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // evita falha do teste por exceções não tratadas do app
-  return false;
-});
